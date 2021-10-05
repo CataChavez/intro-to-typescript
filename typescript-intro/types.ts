c84f30298edc6ddd1f87aa109345bf946211c5f3
cataChavez <catherin.chavez@zentagroup.com>
2021-10-05 14:58:32 -0300

    types

---
+++ typescript-intro/types.ts
@@ -0,0 +1,24 @@
+(function(){ 
+  let message : string = "hola";
+  let number : number = 123;
+  let boolean : boolean = true;
+  let today : Date = new Date();
+
+  let anything;
+  anything = message;
+  anything = number;
+  anything = boolean;
+  anything = today;
+
+  let spiderman = {
+    name: 'Peter',
+    edad: 30
+  }
+
+  spiderman = {
+    name: 'Juan',
+    edad: 30,
+  }
+
+})();
+
