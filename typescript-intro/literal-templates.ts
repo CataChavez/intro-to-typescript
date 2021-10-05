46e07aca61e75fc2faef2ecc31e311502de65c89
cataChavez <catherin.chavez@zentagroup.com>
2021-10-05 14:58:51 -0300

    literal templates

---
+++ typescript-intro/literal-templates.ts
@@ -0,0 +1,15 @@
+(function(){ 
+  
+  function getAge(){
+    return 10+10+13;
+  }
+
+  const name = 'Fernando';
+  const lastName = 'Herrera';
+  const edad = 33;
+
+  const output = `${name} ${lastName} (${ getAge() })`;
+
+  console.log(output)
+})();
+
